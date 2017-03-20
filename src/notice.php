e83be6443394e966ba4dc9d4a8dcadfd6c8f43cf
d-frag <d-frag@shimomo.net>
2017-03-20 16:16:27 +0900

    Fix notice condition

--- src/notice.php
+++ src/notice.php
@@ -5,13 +5,6 @@ require __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set('Asia/Tokyo');
 
 if (! empty($_POST)) {
-    $client = new Maknz\Slack\Client(getenv('SLACK_WEBHOOK_URL'), [
-        'username'   => getenv('SLACK_USERNAME'),
-        'channel'    => getenv('SLACK_CHANNEL'),
-        'icon'       => getenv('SLACK_ICON'),
-        'link_names' => true
-    ]);
-
     ORM::configure('sqlite:./d-frag.db');
     $reports = ORM::for_table('reports')->select('reports.*')->find_many();
 
@@ -26,8 +19,11 @@ if (! empty($_POST)) {
         $text .= "\n";
     }
 
+    $isNotice = false;
     foreach ($reports as $report) {
         if (900 < strtotime($current) - strtotime($report['datetime'])) {
+            $isNotice = true;
+
             $text .= '[Name]';
             $text .= "\n";
             $text .= $report['name'];
@@ -43,7 +39,14 @@ if (! empty($_POST)) {
         }
     }
 
-    if ($text !== '') {
+    if ($isNotice) {
+        $client = new Maknz\Slack\Client(getenv('SLACK_WEBHOOK_URL'), [
+            'username'   => getenv('SLACK_USERNAME'),
+            'channel'    => getenv('SLACK_CHANNEL'),
+            'icon'       => getenv('SLACK_ICON'),
+            'link_names' => true
+        ]);
+
         $client->send($text);
     }
 }
