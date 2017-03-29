db1e2d2adda87a8e95da27e4af7e77e39241c01b
cnc-shimo <shimo@project-com.com>
2017-03-29 18:13:15 +0900

    Add initialization processing

--- src/notice.php
+++ src/notice.php
@@ -6,6 +6,16 @@ date_default_timezone_set('Asia/Tokyo');
 
 if (! empty($_POST)) {
     ORM::configure('sqlite:./d-frag.db');
+    $db = ORM::get_db();
+    $db->exec('
+        CREATE TABLE IF NOT EXISTS reports (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            datetime TEXT,
+            content TEXT
+        );'
+    );
+
     $reports = ORM::for_table('reports')->select('reports.*')->find_many();
 
     $datetime = new DateTimeImmutable();
